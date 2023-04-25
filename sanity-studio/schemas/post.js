ca3285bf38731de0d56387c8bab43b0734873b9f
Taeyooooon <eric568@naver.com>
2023-04-25 18:54:03 +0900

    Add: sanity preview

--- sanity-studio/schemas/post.js
+++ sanity-studio/schemas/post.js
@@ -1,23 +1,23 @@
-export default{
-  title : 'Post',
-  name : 'post',
-  type : 'document',
-  fields : [
+export default {
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
     {
-      title : 'Author',
-      name : 'author',
-      type : 'reference',
-      to : [{type : 'user'}]
+      title: 'Author',
+      name: 'author',
+      type: 'reference',
+      to: [{type: 'user'}],
     },
     {
-      title : "Photo",
-      name : 'photo',
-      type : 'image',
+      title: 'Photo',
+      name: 'photo',
+      type: 'image',
     },
     {
-      title : "Likes",
-      name : 'likes',
-      type : 'array',
+      title: 'Likes',
+      name: 'likes',
+      type: 'array',
       of: [
         {
           type: 'reference',
@@ -27,28 +27,45 @@ export default{
       validation: (Rule) => Rule.unique(),
     },
     {
-      title : "Comments",
-      name : 'comments',
-      type : 'array',
-      of : [
+      title: 'Comments',
+      name: 'comments',
+      type: 'array',
+      of: [
         {
-          title : "Comment",
-          name : 'comment',
-          type : 'document',
-          fields : [
+          title: 'Comment',
+          name: 'comment',
+          type: 'document',
+          fields: [
             {
-              title : 'Author',
-              name : 'author',
-              type : 'reference',
-              to : [{type : 'user'}]
-            }, {
-              title : "Comment",
-              name : 'comment',
-              type : 'string', 
-            }
-          ]
-        }
-      ]
-    }
-  ]
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [{type: 'user'}],
+            },
+            {
+              title: 'Comment',
+              name: 'comment',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'comments.0.comment',
+      authorName: 'author.name',
+      authorUserName: 'author.username',
+      media: 'photo',
+    },
+    prepare(selection) {
+      const {title, authorName, authorUserName, media} = selection
+      return {
+        title,
+        subtitle: `by ${authorName} (${authorUserName})`,
+        media,
+      }
+    },
+  },
 }
